e0719858e6c7a01d1342c77dbfbe3902646db00c
lzptr <26966675+lzptr@users.noreply.github.com>
2023-05-21 15:01:52 +0200

    Added quadrature encoder pio driver. Updated motor driver

--- src/main.c
+++ src/main.c
@@ -1,42 +1,121 @@
+#include <stdio.h>
 #include "SEGGER_RTT.h"
 #include "SEGGER_SYSVIEW.h"
-#include "hardware/irq.h"
-#include "hardware/structs/systick.h"
 #include "hardware/clocks.h"
+#include "hardware/irq.h"
+#include "hardware/pio.h"
 #include "hardware/pwm.h"
+#include "hardware/structs/systick.h"
 #include "pico/stdlib.h"
+#include "quadrature.pio.h"
 
 #define LED_PIN PICO_DEFAULT_LED_PIN
-#define BUTTON_PIN 2
+#define POS_SPEED_BUTTON_PIN 2
+#define NEG_SPEED_BUTTON_PIN 3
+#define DIR_CHANGE_BUTTON_PIN 4
 #define MOTOR_PWM 18
 #define MOTOR_IN1 17
 #define MOTOR_IN2 16
+#define QUADRATURE_A_PIN 14
+#define QUADRATURE_B_PIN 15
+#define DEBOUNCE_TIME 200000u  // ms
+typedef enum Direction
+{
+    LEFT,
+    RIGHT
+} Direction;
+
+PIO pio_;
+uint sm_ = 0u;
+uint8_t currentMotorSpeed_ = 0u;  // duty cycle in pecent
+Direction currentMotorDirection_ = LEFT;
+void setSpeed(uint8_t dutyCycle);
+void setDirection(Direction motorDirection);
 
 void button_gpio_irq()
 {
-    SEGGER_SYSVIEW_RecordEnterISR();
+    static uint32_t posButtonPushedLastTime = 0u;
+    static uint32_t negButtonPushedLastTime = 0u;
+    static uint32_t dirButtonPushedLastTime = 0u;
 
-    gpio_set_irq_enabled(BUTTON_PIN, GPIO_IRQ_EDGE_RISE, false);
-    gpio_acknowledge_irq(BUTTON_PIN, GPIO_IRQ_EDGE_RISE);
-    pwm_set_chan_level( pwm_gpio_to_slice_num(MOTOR_PWM), 0u, 0u); 
-    busy_wait_us(250);
-    gpio_set_irq_enabled(BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true);
+    SEGGER_SYSVIEW_RecordEnterISR();
+    if (gpio_get_irq_event_mask(POS_SPEED_BUTTON_PIN) & GPIO_IRQ_EDGE_RISE)
+    {
+        gpio_acknowledge_irq(POS_SPEED_BUTTON_PIN, GPIO_IRQ_EDGE_RISE);
+
+        // Only react on the edge if the last rising edge occured further away in the past than the dounce time
+        uint32_t currentTime = time_us_32();
+        if (currentTime - posButtonPushedLastTime > DEBOUNCE_TIME)
+        {
+            currentMotorSpeed_ += 10u;
+            if (currentMotorSpeed_ > 100u)
+            {
+                currentMotorSpeed_ = 100u;
+            }
+            setSpeed(currentMotorSpeed_);
+            posButtonPushedLastTime = time_us_32();
+            SEGGER_RTT_WriteString(0, "POS SPEED IRQ\n");
+        }
+    }
+    if (gpio_get_irq_event_mask(NEG_SPEED_BUTTON_PIN) & GPIO_IRQ_EDGE_RISE)
+    {
+        gpio_acknowledge_irq(NEG_SPEED_BUTTON_PIN, GPIO_IRQ_EDGE_RISE);
+
+        // Only react on the edge if the last rising edge occured further away in the past than the dounce time
+        uint32_t currentTime = time_us_32();
+        if (currentTime - negButtonPushedLastTime > DEBOUNCE_TIME)
+        {
+            currentMotorSpeed_ -= 10u;
+            if (currentMotorSpeed_ == 0u || currentMotorSpeed_ > 100u)  // undeflow
+            {
+                currentMotorSpeed_ = 0u;
+            }
+            setSpeed(currentMotorSpeed_);
+            negButtonPushedLastTime = time_us_32();
+            SEGGER_RTT_WriteString(0, "NEG SPEED IRQ\n");
+        }
+    }
+    if (gpio_get_irq_event_mask(DIR_CHANGE_BUTTON_PIN) & GPIO_IRQ_EDGE_RISE)
+    {
+        gpio_acknowledge_irq(DIR_CHANGE_BUTTON_PIN, GPIO_IRQ_EDGE_RISE);
+        uint8_t lastState = gpio_get(DIR_CHANGE_BUTTON_PIN);
+
+        // Only react on the edge if the last rising edge occured further away in the past than the dounce time
+        uint32_t currentTime = time_us_32();
+        if (currentTime - dirButtonPushedLastTime > DEBOUNCE_TIME)
+        {
+            if (currentMotorDirection_ == LEFT)
+            {
+                currentMotorDirection_ = RIGHT;
+            }
+            else
+            {
+                currentMotorDirection_ = LEFT;
+            }
+            setDirection(currentMotorDirection_);
+            dirButtonPushedLastTime = time_us_32();
+            SEGGER_RTT_WriteString(0, "DIR CHANGE SPEED IRQ\n");
+        }
+    }
     SEGGER_SYSVIEW_RecordExitISR();
 }
 
 // Rewrite of weak systick IRQ in crt0.s file
-extern void isr_systick()  
+extern void isr_systick()
 {
     SEGGER_SYSVIEW_RecordEnterISR();
+
+    pio_sm_exec_wait_blocking(pio_, sm_, pio_encode_in(pio_x, 32));
+    uint x = pio_sm_get_blocking(pio_, sm_);
     SEGGER_SYSVIEW_RecordExitISR();
 }
 
 void init_systick()
 {
-    systick_hw->csr = 0;         // Disable
+    systick_hw->csr = 0;        // Disable
     systick_hw->rvr = 124999U;  // Standard System clock (125Mhz)/ (rvr value + 1) = 1ms
-    systick_hw->cvr = 0;         // clear the count to force initial reload
-    systick_hw->csr = 0x7;       // Enable Systic, Enable Exceptions
+    systick_hw->cvr = 0;        // clear the count to force initial reload
+    systick_hw->csr = 0x7;      // Enable Systic, Enable Exceptions
 }
 
 /*********************************************************************
@@ -62,6 +141,40 @@ uint32_t SEGGER_SYSVIEW_X_GetTimestamp(void)
     return timeStamp;
 }
 
+void setSpeed(uint8_t dutyCycle)
+{
+    uint16_t slice_num = pwm_gpio_to_slice_num(MOTOR_PWM);
+    uint16_t duty = dutyCycle;                       // duty cycle, in percent
+    uint16_t level = (0xFFFF - 1) * duty / 100 - 1;  // calculate channel level from given duty cycle in %
+    pwm_set_chan_level(slice_num, 0, level);
+    pwm_set_enabled(slice_num, true);
+}
+
+void setDirection(Direction motorDirection)
+{
+    switch (motorDirection)
+    {
+        case LEFT:
+        {
+            gpio_put(MOTOR_IN1, true);
+            gpio_put(MOTOR_IN2, false);
+            break;
+        }
+        case RIGHT:
+        {
+            gpio_put(MOTOR_IN1, true);
+            gpio_put(MOTOR_IN2, false);
+            break;
+        }
+        default:
+        {
+            // Left
+            gpio_put(MOTOR_IN1, true);
+            gpio_put(MOTOR_IN2, false);
+        }
+    }
+}
+
 int main()
 {
     static char r;
@@ -76,17 +189,24 @@ int main()
     SEGGER_RTT_WriteString(0, "SEGGER Real-Time-Terminal Sample\r\n\r\n");
     SEGGER_RTT_WriteString(0, "###### Testing SEGGER_printf() ######\r\n");
 
-
     // LED
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    // Button Input
-    gpio_init(BUTTON_PIN);
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_acknowledge_irq(BUTTON_PIN, GPIO_IRQ_EDGE_RISE);
-    gpio_set_irq_enabled(BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true);
-    gpio_add_raw_irq_handler(BUTTON_PIN, &button_gpio_irq);
+    // Button Inputs
+    gpio_init(POS_SPEED_BUTTON_PIN);
+    gpio_init(NEG_SPEED_BUTTON_PIN);
+    gpio_set_dir(POS_SPEED_BUTTON_PIN, GPIO_IN);
+    gpio_set_dir(NEG_SPEED_BUTTON_PIN, GPIO_IN);
+    gpio_acknowledge_irq(POS_SPEED_BUTTON_PIN, GPIO_IRQ_EDGE_RISE);
+    gpio_set_irq_enabled(POS_SPEED_BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true);
+    gpio_acknowledge_irq(NEG_SPEED_BUTTON_PIN, GPIO_IRQ_EDGE_RISE);
+    gpio_set_irq_enabled(NEG_SPEED_BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true);
+    gpio_acknowledge_irq(DIR_CHANGE_BUTTON_PIN, GPIO_IRQ_EDGE_RISE);
+    gpio_set_irq_enabled(DIR_CHANGE_BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true);
+    gpio_add_raw_irq_handler(POS_SPEED_BUTTON_PIN, &button_gpio_irq);
+    gpio_add_raw_irq_handler(NEG_SPEED_BUTTON_PIN, &button_gpio_irq);
+    gpio_add_raw_irq_handler(DIR_CHANGE_BUTTON_PIN, &button_gpio_irq);
     irq_set_enabled(IO_IRQ_BANK0, true);
 
     // Motor Driver
@@ -96,29 +216,29 @@ int main()
     gpio_set_dir(MOTOR_IN2, GPIO_OUT);
 
     gpio_set_function(MOTOR_PWM, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(MOTOR_PWM);
+    uint16_t slice_num = pwm_gpio_to_slice_num(MOTOR_PWM);
 
     // PWM Freq. 5 kHz
     int32_t f_sys = clock_get_hz(clk_sys);
-    float divider = f_sys / 25000U;  
+    float divider = f_sys / 25000U;
     pwm_set_clkdiv(slice_num, divider);
     pwm_set_wrap(slice_num, 0xFFFF);
 
-    uint16_t duty = 50; // duty cycle, in percent
-    uint16_t level = (0xFFFF-1) * duty / 100 -1; // calculate channel level from given duty cycle in %
-    pwm_set_chan_level(slice_num, 0, level); 
-    pwm_set_enabled(slice_num, true);
+    // Set default start direction (left)
+    setDirection(currentMotorDirection_);
 
-    int counter = 0;
+    // Quadrature Encoder PIO init. Position get's read every 1ms in systick irq
+    pio_ = pio0;
+    uint16_t offset = pio_add_program(pio_, &quadrature_program);
+    sm_ = pio_claim_unused_sm(pio_, true);
+    quadrature_program_init(pio_, sm_, offset, QUADRATURE_A_PIN, QUADRATURE_B_PIN);
+
     while (true)
     {
-        gpio_put(MOTOR_IN1, true);
-        gpio_put(MOTOR_IN2, false);
         gpio_put(LED_PIN, 1);
-        sleep_us(250000);
+        sleep_us(2500);
         gpio_put(LED_PIN, 0);
-        sleep_us(250000);
-
-        SEGGER_RTT_printf(0, "LED Cycle: %d\n", counter++);
+        sleep_us(2500);
+        printf("%d\n", currentMotorSpeed_);
     }
 }
